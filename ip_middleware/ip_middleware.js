0e13ec1ca5e2e8a0aaf160bf08ea7d25a3e428f1
SaravanakumarJN <kumarjnsaravana@gmail.com>
2021-08-10 16:11:30 +0530

    get ip middleware

--- ip_middleware/ip_middleware.js
+++ ip_middleware/ip_middleware.js
@@ -1,6 +1,18 @@
-const get_ip = (req, res, next) => {
-  let ip = req.headers['x-forwarded-for']?.split(",")[0] || req.socket.remoteAddress
+function ip(callback){
+  // if callback empty or not a function throw error
+  if(callback === undefined || typeof(callback) !== "function"){
+    throw new Error('Callback required')
+  }
 
-  console.log(ip)
-  next()
+  return (req, res, next) => {
+    /*
+      => Along the request, there might be multiple middle points so we get multiple ip 
+      => The origin ip of the request will be the first ip so we get the 0 indexed element
+    */
+    let ip = req.headers['x-forwarded-for']?.split(",")[0] || req.socket.remoteAddress
+  
+    // execute the callback with ip as first argument
+    callback(ip)
+    next()
+  }
 }
